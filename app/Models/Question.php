d1763d6e7fe93db6e677e1c20694b34f9c18ba77
Abdullah Alhaj <torgodly@gmail.com>
2024-10-04 16:09:02 +0200

    first

---
+++ app/Models/Question.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    /** @use HasFactory<\Database\Factories\QuestionFactory> */
+    use HasFactory;
+
+    protected $fillable = ['body', 'user_id', 'answer', 'answered_by', 'answered_at'];
+}
